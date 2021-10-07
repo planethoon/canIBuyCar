8cd7af95d07a93762797bfbcbea84c6fe2dc2059
glen <glen910115@gmail.com>
2021-10-07 23:55:32 +0900

    SETTING : make router and controllers path

---
+++ server/controllers/index.js
@@ -0,0 +1,6 @@
+module.exports = {
+  authController: require('./auth'),
+  carController: require('./car'),
+  boardController: require('./board'),
+  bookmarkController: require('./bookmark'),
+};
