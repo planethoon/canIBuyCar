153af97608cdae58640ace59133b05ee95396b09
glen <glen910115@gmail.com>
2021-10-08 18:56:27 +0900

    ADD : bookmark create

--- server/routers/board.js
+++ server/routers/board.js
@@ -3,8 +3,8 @@ const router = express.Router();
 
 const {boardController} = require('../controllers');
 
-router.post('/', boardController.create.post);
-router.get('/', boardController.getAll.get);
-router.delete('/:id', boardController.remove.delete);
+router.post('/', boardController.create);
+// router.get('/', boardController.getAll);
+// router.delete('/:id', boardController.remove);
 
 module.exports = router;
