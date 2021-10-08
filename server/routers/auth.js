3009b6e3630080362aa9eee5b861557ac72b2cdf
glen <glen910115@gmail.com>
2021-10-08 11:13:58 +0900

    ADD : signup

--- server/routers/auth.js
+++ server/routers/auth.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const {authController} = require('../controllers');
 
 router.get('/', authController.sayhi);
-// router.post('/signup', authController.signup.post);
+router.post('/signup', authController.signup);
 router.post('/login', authController.login);
 // router.post('/logout', authController.logout.post);
 // router.put('/', authController.edit.put);
