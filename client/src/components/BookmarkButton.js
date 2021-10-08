6629df12690b9379c7a7d16e9d4374d7eee2cbad
planethoon <planethoon@hotmail.com>
2021-10-08 22:42:49 +0900

    FIX: fix brand flexbox

---
+++ client/src/components/BookmarkButton.js
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+import marked from "../img/marked.png";
+import unmarked from "../img/unmarked.png";
+
+const BookmarkWrapper = styled.div`
+  height: 2rem;
+  width: 2rem;
+  > img {
+    max-width: 100%;
+  }
+`;
+
+export default function BookmarkButton() {
+  return (
+    <>
+      <BookmarkWrapper>
+        <img src={marked} alt="" />
+      </BookmarkWrapper>
+    </>
+  );
+}
