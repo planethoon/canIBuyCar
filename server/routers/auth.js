14d5e0191fff1df045deae5bd35404e404b59d00
Eom-Hyejin <qnwkdhk9192@gmail.com>
2021-10-08 11:39:55 +0900

    ADD : add function logout, edit, remove

--- server/routers/auth.js
+++ server/routers/auth.js
@@ -3,8 +3,8 @@ const router = express.Router();
 
 const {authController} = require('../controllers');
 
-// router.post('/signup', authController.signup.post);
-// router.post('/login', authController.login.post);
+router.post('/signup', authController.signup.post);
+router.post('/login', authController.login.post);
 router.post('/logout', authController.logout);
 router.put('/', authController.edit);
 router.delete('/', authController.remove.delete);
