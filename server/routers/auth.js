05f9a19b8e9214d003008db3c2243586a3cabe1e
glen <glen910115@gmail.com>
2021-10-08 11:57:56 +0900

    FIX : undo comments in router/auth.js

--- server/routers/auth.js
+++ server/routers/auth.js
@@ -6,8 +6,8 @@ const {authController} = require('../controllers');
 router.get('/', authController.sayhi);
 router.post('/signup', authController.signup);
 router.post('/login', authController.login);
-// router.post('/logout', authController.logout.post);
-// router.put('/', authController.edit.put);
-// router.delete('/', authController.remove.delete);
+router.post('/logout', authController.logout);
+router.put('/', authController.edit);
+router.delete('/', authController.remove);
 
 module.exports = router;
