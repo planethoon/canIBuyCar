26a96eb810cec24cfda562a8573da2bbe9288a44
glen <glen910115@gmail.com>
2021-10-08 19:14:09 +0900

    FIX : add jwt verify in bookmark create

--- server/controllers/bookmark/create.js
+++ server/controllers/bookmark/create.js
@@ -1,12 +1,26 @@
 const {Users_car} = require('../../models');
+const {User} = require('../../models');
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
 
 module.exports = async (req, res) => {
-  const {userId, carId} = req.body;
-  const bookmark = await Users_car.create({userId, carId});
-  //생성하지만 userId, carId는 원래 스키마에 없어서인지 들어가지 않는다.
-  console.log(bookmark);
+  const authorization = req.headers.authorization;
+  if (!authorization) {
+    res.status(403).send({message: '로그인이 필요합니다'});
+  } else {
+    const token = req.headers.authorization.split('Bearer ')[1];
+    const data = jwt.verify(token, process.env.ACCESS_SECRET);
+    const userInfo = await User.findOne({
+      where: {email: data.email},
+    });
 
-  res.json({data: {bookmark}, message: '즐겨찾기 완료'});
+    if (userInfo) {
+      const carId = req.body;
+      const userId = userInfo.dataValues.id;
+      const bookmark = await Users_car.create({userId, carId});
+      //생성하지만 userId, carId는 원래 스키마에 없어서인지 들어가지 않는다.
+      console.log(bookmark);
+      res.json({data: {bookmark}, message: '즐겨찾기 완료'});
+    }
+  }
 };
