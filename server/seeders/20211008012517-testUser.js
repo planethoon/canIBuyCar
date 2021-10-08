31eb20b5772441b08e66813088337f70c739e4af
glen <glen910115@gmail.com>
2021-10-08 10:28:39 +0900

    ADD : testUser seed

---
+++ server/seeders/20211008012517-testUser.js
@@ -0,0 +1,65 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+     */
+    const testUser = [
+      {
+        email: 'testA.gmail.com',
+        username: 'testA',
+        password: '1234',
+        createdAt: new Date()
+          .toISOString()
+          .replace(/T/, ' ')
+          .replace(/\..+/, ''),
+        updatedAt: new Date()
+          .toISOString()
+          .replace(/T/, ' ')
+          .replace(/\..+/, ''),
+      },
+      {
+        email: 'testB.gmail.com',
+        username: 'testB',
+        password: '1234',
+        createdAt: new Date()
+          .toISOString()
+          .replace(/T/, ' ')
+          .replace(/\..+/, ''),
+        updatedAt: new Date()
+          .toISOString()
+          .replace(/T/, ' ')
+          .replace(/\..+/, ''),
+      },
+      {
+        email: 'testC.gmail.com',
+        username: 'testC',
+        password: '1234',
+        createdAt: new Date()
+          .toISOString()
+          .replace(/T/, ' ')
+          .replace(/\..+/, ''),
+        updatedAt: new Date()
+          .toISOString()
+          .replace(/T/, ' ')
+          .replace(/\..+/, ''),
+      },
+    ];
+    return queryInterface.bulkInsert('Users', testUser, {});
+  },
+  down: async (queryInterface, Sequelize) => {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  },
+};
