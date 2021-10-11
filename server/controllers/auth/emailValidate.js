ccebebfe7cad0b849ab89b15e2905c3f1d43fdbe
glen <glen910115@gmail.com>
2021-10-11 22:49:19 +0900

    FIX : emailValidate

---
+++ server/controllers/auth/emailValidate.js
@@ -0,0 +1,14 @@
+const {User} = require('../../models');
+
+module.exports = async (req, res) => {
+  const email = req.body.email;
+  const user = await User.findOne({
+    where: {email},
+  });
+  console.log(user);
+  if (user) {
+    return res.status(409).json({message: '이미 존재하는 이메일입니다.'});
+  } else {
+    return res.status(200).json({message: '가입 가능한 이메일입니다'});
+  }
+};
