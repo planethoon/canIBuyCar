c231ed789516c73604b5a15439dba5ac2534268f
glen <glen910115@gmail.com>
2021-10-11 22:17:25 +0900

    ADD : email validate

--- server/controllers/auth/signup.js
+++ server/controllers/auth/signup.js
@@ -1,15 +1,23 @@
 const {User} = require('../../models');
 
-module.exports = async (req, res) => {
-  const {email, password, username} = req.body;
-  const user = await User.findOne({
-    where: {email},
-  });
-  if (user) {
-    return res.status(409).json({message: '이미 존재하는 이메일입니다.'});
-  }
-  const userData = await User.create({email, password, username}).then(
-    (data) => data.dataValues
-  );
-  res.json({data: {userId: userData.id, userName: userData.username}});
+module.exports = {
+  emailValidate: async (req, res) => {
+    const email = req.body.email;
+    const user = await User.findOne({
+      where: {email},
+    });
+    if (user) {
+      return res.status(409).json({message: '이미 존재하는 이메일입니다.'});
+    } else {
+      return res.status(200).json({message: '가입 가능한 이메일입니다'});
+    }
+  },
+  create: async (req, res) => {
+    const {email, password, username} = req.body;
+
+    const userData = await User.create({email, password, username}).then(
+      (data) => data.dataValues
+    );
+    res.json({data: {userId: userData.id, userName: userData.username}});
+  },
 };
