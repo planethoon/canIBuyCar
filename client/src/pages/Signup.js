0d8d224dc1d673debe3d61fc9a1765e5afab6d33
ydh94 <dbeogud9411@gmail.com>
2021-10-11 19:51:43 +0900

    ADD : check validation

--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -60,11 +60,11 @@ const ValidationBox = styled(StyledDiv)`
 
 export default function Signup() {
   function isEmail(asValue) {
-    var regExp = /^[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/i;
+    var regExp = /^[0-9a-zA-Z]([-_]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/i;
     return regExp.test(asValue);
   }
   function isPassword(asValue) {
-    var regExp = /^(?=.*\d)(?=.*[a-zA-Z])[0-9a-zA-Z]{8,16}$/; //  8 ~ 10자 영문, 숫자 조합
+    var regExp = /^(?=.*\d)(?=.*[a-zA-Z])[0-9a-zA-Z]{8,16}$/;
     return regExp.test(asValue);
   }
 
