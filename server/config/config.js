79153370c906e778f3526c7eef9cda031414036a
glen <glen910115@gmail.com>
2021-10-12 15:43:26 +0900

    TEST : cloud var 2

--- server/config/config.js
+++ server/config/config.js
@@ -3,26 +3,29 @@ const env = process.env;
 
 const development = {
   username: env.DATABASE_USER,
-  password: null,
+  password: env.DATABASE_PASSWORD,
   database: 'canIBuyCar',
   host: env.DATABASE_HOST,
   dialect: 'mysql',
+  port: env.DATABASE_PORT,
 };
 
 const production = {
   username: env.DATABASE_USER,
-  password: null,
+  password: env.DATABASE_PASSWORD,
   database: 'canIBuyCar',
   host: env.DATABASE_HOST,
   dialect: 'mysql',
+  port: env.DATABASE_PORT,
 };
 
 const test = {
   username: env.DATABASE_USER,
-  password: null,
+  password: env.DATABASE_PASSWORD,
   database: 'canIBuyCar',
   host: env.DATABASE_HOST,
   dialect: 'mysql',
+  port: env.DATABASE_PORT,
 };
 
 module.exports = {development, production, test};
