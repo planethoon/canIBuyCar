c09373b2288ca663fcfc91be7aa47c7701f5fc09
Eom-Hyejin <qnwkdhk9192@gmail.com>
2021-10-14 01:57:50 +0900

    FIX : fix book getAll

--- server/controllers/bookmark/getAll.js
+++ server/controllers/bookmark/getAll.js
@@ -1,4 +1,6 @@
 const {User} = require('../../models');
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
 
 module.exports = async (req, res) => {
   const authorization = req.headers.authorization;
